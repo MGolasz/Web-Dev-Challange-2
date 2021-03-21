1238ab137197d3dab58c102ebba9e91204a2bea7
Shenter666 <77466376+Shenter666@users.noreply.github.com>
2021-03-21 23:13:00 +0100

    Hamburger icon changing

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
     const hamburger = document.querySelector('#hamburger');
     const nav = document.querySelector('#nav');
     const body = document.querySelector('body');
+    const img = document.querySelector('#hamburger-icon');
+    
 
     hamburger.addEventListener('click', () => {
         nav.classList.toggle('show');
         body.classList.toggle('scroll-lock');
+
+        if (img.src.match('/img/fi-rr-align-justify.svg')) {
+            img.src = '/img/fi-rr-cross.svg';
+        }
+        else {
+            img.src = '/img/fi-rr-align-justify.svg';
+        }
     })
 
