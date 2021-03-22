93bc5e9b6ee8338cdd9178bf289448b211900f12
Shenter666 <77466376+Shenter666@users.noreply.github.com>
2021-03-22 11:33:40 +0100

    Fully workign hamburger menu + mobile contact fix

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-    const hamburger = document.querySelector('#hamburger');
-    const nav = document.querySelector('#nav');
-    const body = document.querySelector('body');
-    const img = document.querySelector('#hamburger-icon');
+const hamburger = document.querySelector('#hamburger');
+const nav = document.querySelector('#nav');
+const body = document.querySelector('body');
+const img = document.querySelector('#hamburger-icon');
+const navLink = document.querySelectorAll('.nav-link');
+console.log(typeof Array.from(navLink));
+
+const burgerChange = () => {
+    if (nav.classList.contains('show')) {
+        img.src = '/img/fi-rr-cross.svg';
+    }
+    else {
+        img.src = '/img/fi-rr-align-justify.svg';
+    }
+}
+
+hamburger.addEventListener('click', () => {
+    nav.classList.toggle('show');
+    body.classList.toggle('scroll-lock');
+    burgerChange();
+})
+
+navLink.forEach(el => {
+    el.addEventListener('click', () => {
+        body.classList.remove('scroll-lock');
+        nav.classList.remove('show');
+        burgerChange();
+    })}
+);
+    
     
 
-    hamburger.addEventListener('click', () => {
-        nav.classList.toggle('show');
-        body.classList.toggle('scroll-lock');
-
-        if (img.src.match('/img/fi-rr-align-justify.svg')) {
-            img.src = '/img/fi-rr-cross.svg';
-        }
-        else {
-            img.src = '/img/fi-rr-align-justify.svg';
-        }
-    })
 
